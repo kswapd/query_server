c999dd73579a1544d87299f4bbf98d852e835907
likun <kkkun_li@163.com>
2016-12-30 16:58:42 +0800

    修复uuid等指标未进入输出的bug

--- Monitor/MySQLMonitor.go
+++ Monitor/MySQLMonitor.go
@@ -151,37 +151,46 @@ func parseMySQLResult(res []client.Result) AppMySQLJson {
 	//container_uuid
 	var container_uuid string
 	indexOfUuid := indexOf(res[0].Series[0].Columns, "container_uuid")
-	if res[0].Series[0].Values[0][indexOfUuid] != nil {
-		container_uuid = res[0].Series[0].Values[0][indexOfUuid].(string)
+	i := 0
+	for res[0].Series[0].Values[i][indexOfUuid] == nil {
+		i++
 	}
+	container_uuid = res[0].Series[0].Values[i][indexOfUuid].(string)
+	//	if res[0].Series[0].Values[0][indexOfUuid] != nil {
+	//		container_uuid = res[0].Series[0].Values[0][indexOfUuid].(string)
+	//	}
 
 	//environment_id
 	var environment_id string
 	indexOfId := indexOf(res[0].Series[0].Columns, "environment_id")
-	if res[0].Series[0].Values[0][indexOfId] != nil {
-		environment_id = res[0].Series[0].Values[0][indexOfId].(string)
-	}
+	environment_id = res[0].Series[0].Values[i][indexOfId].(string)
+	//	if res[0].Series[0].Values[0][indexOfId] != nil {
+	//		environment_id = res[0].Series[0].Values[0][indexOfId].(string)
+	//	}
 
 	//container_name
 	var container_name string
 	indexOfName := indexOf(res[0].Series[0].Columns, "container_name")
-	if res[0].Series[0].Values[0][indexOfName] != nil {
-		container_name = res[0].Series[0].Values[0][indexOfName].(string)
-	}
+	container_name = res[0].Series[0].Values[i][indexOfName].(string)
+	//	if res[0].Series[0].Values[0][indexOfName] != nil {
+	//		container_name = res[0].Series[0].Values[0][indexOfName].(string)
+	//	}
 
 	//namespace
 	var namespace string
 	indexOfNamespace := indexOf(res[0].Series[0].Columns, "namespace")
-	if res[0].Series[0].Values[0][indexOfNamespace] != nil {
-		namespace = res[0].Series[0].Values[0][indexOfNamespace].(string)
-	}
+	namespace = res[0].Series[0].Values[i][indexOfNamespace].(string)
+	//	if res[0].Series[0].Values[0][indexOfNamespace] != nil {
+	//		namespace = res[0].Series[0].Values[0][indexOfNamespace].(string)
+	//	}
 
 	//type
 	var appType string
 	indexOfType := indexOf(res[0].Series[0].Columns, "type")
-	if res[0].Series[0].Values[0][indexOfType] != nil {
-		appType = res[0].Series[0].Values[0][indexOfType].(string)
-	}
+	appType = res[0].Series[0].Values[i][indexOfType].(string)
+	//	if res[0].Series[0].Values[0][indexOfType] != nil {
+	//		appType = res[0].Series[0].Values[0][indexOfType].(string)
+	//	}
 	var amqr []AppMySQLQueryResult
 	for _, v := range timeStat {
 		var qrd AppMySQLQueryResultData
--- Monitor/RedisMonitor.go
+++ Monitor/RedisMonitor.go
@@ -11,9 +11,9 @@ import (
 //原理参照nginx
 func parseRedisResult(res []client.Result) AppRedisJson {
 
-	var appRedisJson AppRedisJson
+	//	fmt.Println(res)
 
-	//	fmt.Println("############################", len(res))
+	var appRedisJson AppRedisJson
 
 	if len(res) == 0 {
 		return appRedisJson
@@ -27,16 +27,13 @@ func parseRedisResult(res []client.Result) AppRedisJson {
 		redisResult[v.Name] = make(map[string]float64)
 		index := indexOf(v.Columns, Value_key_of_last)
 
-		//		fmt.Println(index)
-
 		for _, v1 := range v.Values {
-			//			fmt.Println(len(v1))
 			if v1[index] == nil {
 				continue
 			}
+
 			f64, _ := strconv.ParseFloat(string(v1[index].(json.Number)), 64)
 			redisResult[v.Name][v1[0].(string)] = f64
-
 		}
 	}
 
@@ -200,37 +197,63 @@ func parseRedisResult(res []client.Result) AppRedisJson {
 	//container_uuid
 	var container_uuid string
 	indexOfUuid := indexOf(res[0].Series[0].Columns, "container_uuid")
-	if res[0].Series[0].Values[0][indexOfUuid] != nil {
-		container_uuid = res[0].Series[0].Values[0][indexOfUuid].(string)
+
+	i := 0
+	for res[0].Series[0].Values[i][indexOfUuid] == nil {
+		i++
 	}
+	container_uuid = res[0].Series[0].Values[i][indexOfUuid].(string)
+	//	if res[0].Series[0].Values[i][indexOfUuid] != nil {
+	//		container_uuid = res[0].Series[0].Values[i][indexOfUuid].(string)
+	//	}
 
 	//environment_id
 	var environment_id string
 	indexOfId := indexOf(res[0].Series[0].Columns, "environment_id")
-	if res[0].Series[0].Values[0][indexOfId] != nil {
-		environment_id = res[0].Series[0].Values[0][indexOfId].(string)
-	}
+	//	i = 0
+	//	for res[0].Series[0].Values[i][indexOfId] == nil {
+	//		i++
+	//	}
+	environment_id = res[0].Series[0].Values[i][indexOfId].(string)
+	//	if res[0].Series[0].Values[0][indexOfId] != nil {
+	//		environment_id = res[0].Series[0].Values[i][indexOfId].(string)
+	//	}
 
 	//container_name
 	var container_name string
 	indexOfName := indexOf(res[0].Series[0].Columns, "container_name")
-	if res[0].Series[0].Values[0][indexOfName] != nil {
-		container_name = res[0].Series[0].Values[0][indexOfName].(string)
-	}
+	//	i = 0
+	//	for res[0].Series[0].Values[i][indexOfName] == nil {
+	//		i++
+	//	}
+	container_name = res[0].Series[0].Values[i][indexOfName].(string)
+	//	if res[0].Series[0].Values[0][indexOfName] != nil {
+	//		container_name = res[0].Series[0].Values[0][indexOfName].(string)
+	//	}
 
 	//namespace
 	var namespace string
 	indexOfNamespace := indexOf(res[0].Series[0].Columns, "namespace")
-	if res[0].Series[0].Values[0][indexOfNamespace] != nil {
-		namespace = res[0].Series[0].Values[0][indexOfNamespace].(string)
-	}
+	//	i = 0
+	//	for res[0].Series[0].Values[i][indexOfNamespace] == nil {
+	//		i++
+	//	}
+	namespace = res[0].Series[0].Values[i][indexOfNamespace].(string)
+	//	if res[0].Series[0].Values[0][indexOfNamespace] != nil {
+	//		namespace = res[0].Series[0].Values[0][indexOfNamespace].(string)
+	//	}
 
 	//type
 	var appType string
 	indexOfType := indexOf(res[0].Series[0].Columns, "type")
-	if res[0].Series[0].Values[0][indexOfType] != nil {
-		appType = res[0].Series[0].Values[0][indexOfType].(string)
-	}
+	//	i = 0
+	//	for res[0].Series[0].Values[i][indexOfType] == nil {
+	//		i++
+	//	}
+	appType = res[0].Series[0].Values[i][indexOfType].(string)
+	//	if res[0].Series[0].Values[0][indexOfType] != nil {
+	//		appType = res[0].Series[0].Values[0][indexOfType].(string)
+	//	}
 
 	var arqr []AppRedisQueryResult
 	for _, v := range timeStat {
