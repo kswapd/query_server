88dc2373e7dea5bcf74d38fb5073c81f9418c148
kongxiangwen <xw.kong@hnair.com>
2017-01-18 13:08:09 +0800

    ok

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
 GOOS=linux go build -o bin/monitor-query-server
 docker build -t registry.hnaresearch.com/public/monitor-query-server:1.1 .
-#docker push registry.hnaresearch.com/public/monitor-query-server:1.0
+#docker push registry.hnaresearch.com/public/monitor-query-server:1.1
