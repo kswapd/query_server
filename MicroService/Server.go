ace621f865dabd31dab966d7a2d128d2e416cd58
kongxiangwen <xw.kong@hnair.com>
2017-02-09 15:01:36 +0800

    server revert

--- MicroService/Server.go
+++ MicroService/Server.go
@@ -1,37 +1,42 @@
 package MicroService
-
 import (
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
-func Start() {
-	r := gin.Default()
-	r.GET("/ping", OnPing)
-	r.POST("/queryLogInfo", OnQueryLogInfo)
-	r.GET("/queryLogInfo", OnQueryLogInfo)
 
-	r.GET("queryCustomLogFile", OnQueryLogInfo)
-	r.POST("queryCustomLogFile", OnQueryLogInfo)
+func Start(){
+    r := gin.Default()
+    r.GET("/ping", OnPing)
+    r.POST("/queryLogInfo", OnQueryLogInfo)
+    r.GET("/queryLogInfo", OnQueryLogInfo)
 
-	r.POST("/queryMonitorInfo", OnQueryMonitorInfo)
-	r.GET("/queryMonitorInfo", OnQueryMonitorInfo)
+    r.POST("/queryMonitorInfo",OnQueryMonitorInfo)
+    r.GET("/queryMonitorInfo",OnQueryMonitorInfo)
 
-	r.GET("/logyun/selectPerformanceInfo.do", OnQueryMonitorInfo)
-	r.POST("/logyun/selectPerformanceInfo.do", OnQueryMonitorInfo)
-	r.GET("/logyun/selectLogInfo.do", OnQueryLogInfo)
 
-	r.GET("/queryContainerStatus", OnQueryContainerStatus)
-	r.POST("/queryContainerStatus", OnQueryContainerStatus)
 
-	/*r.GET("/queryMonitorInfo", func(c *gin.Context) {
+    r.GET("/logyun/selectPerformanceInfo.do", OnQueryMonitorInfo)
+    r.POST("/logyun/selectPerformanceInfo.do", OnQueryMonitorInfo)
 
-	    id := c.Query("id")
-	    page := c.DefaultQuery("page", "0")
-	    name := c.PostForm("name")
-	    message := c.PostForm("message")
+    r.GET("/logyun/selectLogInfo.do",OnQueryLogInfo)
+    r.POST("/logyun/selectLogInfo.do",OnQueryLogInfo)
 
-	    fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
-	})*/
-	//r.Run() // listen and serve on 0.0.0.0:8080
-	r.Run(":8100")
+    r.GET("/logyun/searchCustomInfo.do",OnQueryCustomInfo)       //todo
+    r.POST("/logyun/searchCustomInfo.do",OnQueryCustomInfo)
+
+
+
+    r.GET("/queryContainerStatus",OnQueryContainerStatus)
+    r.POST("/queryContainerStatus",OnQueryContainerStatus)
+    /*r.GET("/queryMonitorInfo", func(c *gin.Context) {
+
+        id := c.Query("id")
+        page := c.DefaultQuery("page", "0")
+        name := c.PostForm("name")
+        message := c.PostForm("message")
+
+        fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+    })*/
+    //r.Run() // listen and serve on 0.0.0.0:8080
+    r.Run(":8059")
 }
