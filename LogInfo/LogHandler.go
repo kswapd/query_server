98b33537c019a738ed83f536345a16e9595e81d8
likun <kkkun_li@163.com>
2017-02-09 13:49:40 +0800

    添加自定义日志查询

--- LogInfo/LogHandler.go
+++ LogInfo/LogHandler.go
@@ -204,6 +204,175 @@ func QueryAppLog(c *gin.Context, queryInfo Common.QueryLogJson) {
 	q = q.Must(elastic.NewMatchQuery("data.container_uuid", queryInfo.Container_uuid))
 	q = q.Must(elastic.NewRangeQuery("data.log_info.log_time").Gt(queryInfo.Start_time).Lt(queryInfo.End_time))
 
+	//	q = q.Must(elastic.NewMatchQuery("data.app_file", queryInfo.File_name))
+
+	// q = q.Must(elastic.NewMatchQuery("data.environment_id", "Network Agent"))
+
+	//dt := elastic.NewRangeQuery("data.log_info.log_time").Gt(queryInfo.Start_time).Lt(queryInfo.End_time)
+	// q = q.Must(elastic.NewRangeQuery("data.log_infoq = q.Must(elastic.NewTermQuery("data.container_uuid", queryInfo.Container_uuid)).log_time").Gt(queryInfo.Start_time).Lt(queryInfo.End_time))
+
+	src, err := q.Source()
+	if err != nil {
+		c.JSON(200, ErrElasticsearch)
+		return
+
+	}
+	data, err := json.Marshal(src)
+	if err != nil {
+		c.JSON(200, ErrElasticsearch)
+		return
+	}
+	ss := string(data)
+	fmt.Println(ss)
+
+	search := client.Search().Index("fluentd_from_*_to_es.log-*") //.Type("film")
+	search = search.Query(q)                                      //.Filter(andFilter)
+
+	search = search.From(pageIndex - 1).Size(lengthPerPage)
+
+	searchResult, err := search.Do(context.TODO())
+
+	if err != nil {
+		c.JSON(200, ErrElasticsearch)
+		return
+	}
+	fmt.Printf("Found a total of %d ,%d result, took %d milliseconds.\n", searchResult.TotalHits(), searchResult.Hits.TotalHits, searchResult.TookInMillis)
+
+	var appType string
+	//	var s SContainerLogger
+
+	var logNginxResult SQueryNginxLogResult
+	var logRedisResult SQueryRedisLogResult
+	var logMysqlResult SQueryMysqlLogResult
+
+	//var t SNginxLogger
+	//var t interface{}
+	var tNginx SNginxLogger
+	var tMysql SMysqlLogger
+	var tRedis SRedisLogger
+
+	/*logResult.Return_code = 200
+	  logResult.Current_query_result_length = 10
+	  logResult.All_query_result_length = 100*/
+
+	if len(searchResult.Hits.Hits) > 0 {
+
+		//		err := json.Unmarshal(*searchResult.Hits.Hits[0].Source, &s)
+
+		//		if err != nil {
+		//			// Deserialization failed
+		//		}
+
+		//		appType = s.Type
+
+		var d interface{}
+		err := json.Unmarshal(*searchResult.Hits.Hits[0].Source, &d)
+		if err != nil {
+			// Deserialization failed
+		}
+		appType = (d.(map[string]interface{}))["type"].(string)
+		fmt.Printf("App type:%s.\n", appType)
+
+		if strings.Contains(appType, "nginx") {
+			t := tNginx
+			logResult := logNginxResult
+
+			logResult.Return_code = 200
+			logResult.All_query_result_length = searchResult.Hits.TotalHits
+			logResult.Current_query_result_length = int64(len(searchResult.Hits.Hits))
+			for _, hit := range searchResult.Hits.Hits {
+				err := json.Unmarshal(*hit.Source, &t)
+				if err != nil {
+				}
+				logResult.Query_result = append(logResult.Query_result, t)
+			}
+			c.JSON(200, logResult)
+		} else if strings.Contains(appType, "mysql") {
+			t := tMysql
+			logResult := logMysqlResult
+			logResult.Return_code = 200
+			logResult.All_query_result_length = searchResult.Hits.TotalHits
+			logResult.Current_query_result_length = int64(len(searchResult.Hits.Hits))
+			for _, hit := range searchResult.Hits.Hits {
+				err := json.Unmarshal(*hit.Source, &t)
+				if err != nil {
+				}
+				logResult.Query_result = append(logResult.Query_result, t)
+			}
+			c.JSON(200, logResult)
+
+		} else if strings.Contains(appType, "redis") {
+			t := tRedis
+			logResult := logRedisResult
+			logResult.Return_code = 200
+			logResult.All_query_result_length = searchResult.Hits.TotalHits
+			logResult.Current_query_result_length = int64(len(searchResult.Hits.Hits))
+			for _, hit := range searchResult.Hits.Hits {
+				err := json.Unmarshal(*hit.Source, &t)
+				if err != nil {
+				}
+				logResult.Query_result = append(logResult.Query_result, t)
+			}
+			c.JSON(200, logResult)
+
+		} else {
+			c.JSON(200, QueryNoResult)
+			return
+		}
+
+	} else {
+		c.JSON(200, QueryNoResult)
+	}
+
+}
+func QueryCustomLog(c *gin.Context, queryInfo Common.QueryLogJson) {
+
+	client, err := elastic.NewClient(elastic.SetURL(ESUrl))
+	pageIndex := 0
+	lengthPerPage := 50
+	if err != nil {
+		c.JSON(200, ConnElasticsearchErr)
+		return
+
+	}
+
+	if queryInfo.Container_uuid == "" || queryInfo.Start_time == "" || queryInfo.End_time == "" || queryInfo.Page_index == "" || queryInfo.Length_per_page == "" {
+		c.JSON(200, InvalidQuery)
+		return
+	}
+
+	if pageIndex, err = strconv.Atoi(queryInfo.Page_index); err != nil {
+		c.JSON(200, InvalidQuery)
+		return
+	}
+
+	if lengthPerPage, err = strconv.Atoi(queryInfo.Length_per_page); err != nil {
+		c.JSON(200, InvalidQuery)
+		return
+	}
+
+	/* count, err := client.Count("fluentd_from_container_to_es.log-2016.12.01").Do(context.TODO())
+	   if err != nil {
+	     fmt.Printf("error:%#v.\n",err)
+	   }
+
+	   fmt.Printf("No condition got %d.\n",  count)
+
+
+	   fmt.Println(queryInfo.Container_uuid)*/
+
+	q := elastic.NewBoolQuery()
+
+	//      q = q.Must(elastic.NewTermQuery("data.container_uuid", queryInfo.Container_uuid))
+
+	q = q.Must(elastic.NewMatchQuery("type", "custom_log"))
+	//q = q.Must(elastic.NewMatchQuery("data.log_info.source", "stdout"))
+	//q = q.Should(elastic.NewTermQuery("type", "log_container"))
+	q = q.Must(elastic.NewMatchQuery("data.container_uuid", queryInfo.Container_uuid))
+	q = q.Must(elastic.NewRangeQuery("data.log_info.log_time").Gt(queryInfo.Start_time).Lt(queryInfo.End_time))
+
+	q = q.Must(elastic.NewMatchQuery("data.app_file", queryInfo.File_name))
+
 	// q = q.Must(elastic.NewMatchQuery("data.environment_id", "Network Agent"))
 
 	//dt := elastic.NewRangeQuery("data.log_info.log_time").Gt(queryInfo.Start_time).Lt(queryInfo.End_time)
@@ -351,6 +520,11 @@ func QueryCustomLogFile(c *gin.Context, queryInfo Common.QueryLogJson) {
 	}
 
 	//	elastic.NewTermsAggregation()
+	//	ta := elastic.NewTermsAggregation() //terms aggregation，
+	//	ta.Field("app_file")
+
+	termAggregation := elastic.NewTermsAggregation()
+	termAggregation.Field("app_file")
 
 	q := elastic.NewBoolQuery()
 
@@ -435,7 +609,10 @@ func QueryLogInfo(c *gin.Context) {
 	queryInfo.Query_content = c.Query("query_content")
 	queryInfo.Length_per_page = c.Query("length_per_page")
 	queryInfo.Page_index = c.Query("page_index")
-	queryInfo.File_name = c.Query("file_name")
+
+	if fileName := c.Query("file_name"); fileName != "" {
+		queryInfo.File_name = c.Query("file_name")
+	}
 
 	//c.BindJSON(&queryInfo)
 	//c.JSON(200, gin.H{"type": queryInfo.Query_type})
@@ -448,7 +625,7 @@ func QueryLogInfo(c *gin.Context) {
 	case "app":
 		QueryAppLog(c, queryInfo)
 	case "custom_log":
-		QueryCustomLogFile(c, queryInfo)
+		QueryCustomLog(c, queryInfo)
 	default:
 		c.JSON(200, InvalidQuery)
 		return
